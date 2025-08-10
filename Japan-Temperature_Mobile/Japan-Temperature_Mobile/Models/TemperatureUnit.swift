a1ab67c83c83d80b4308d0669fbde82e89e037c6
gadgeski <xtwh.ce5bil@gmail.com>
2025-08-10 18:08:00 +0900

    Created TemperatureUnit.swift

---
+++ Japan-Temperature_Mobile/Japan-Temperature_Mobile/Models/TemperatureUnit.swift
@@ -0,0 +1,13 @@
+//
+//  TemperatureUnit.swift
+//  Japan-Temperature_Mobile
+//
+//  Created by Dev Tech on 2025/08/10.
+//
+
+import Foundation
+
+enum TemperatureUnit: String, CaseIterable {
+    case celsius
+    case fahrenheit
+}
