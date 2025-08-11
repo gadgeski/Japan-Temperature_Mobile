36ff7599dbbc770a92f7ae2f3dd80b62bd851c04
gadgeski <xtwh.ce5bil@gmail.com>
2025-08-11 21:11:13 +0900

    Created FavoriteCity.swift

---
+++ Japan-Temperature_Mobile/Japan-Temperature_Mobile/Models/FavoriteCity.swift
@@ -0,0 +1,13 @@
+//
+//  FavoriteCity.swift
+//  Japan-Temperature_Mobile
+//
+//  Created by Dev Tech on 2025/08/10.
+//
+
+import Foundation
+
+struct FavoriteCity: Identifiable {
+    var id: String { cityName }
+    let cityName: String
+}
